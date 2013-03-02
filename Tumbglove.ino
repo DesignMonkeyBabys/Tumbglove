597778717809f9516cbeeac015fd8aa702abe2b0
satoshin2071 <satoshin2071@gmail.com>
2013-03-02 21:03:02 +0900

    reblog機能を追加

--- Tumbglove.ino
+++ Tumbglove.ino
@@ -27,24 +27,43 @@
 
 // Flex sensor pin
 const int sensorPin = 0;
-
 int currentValue = 0;
 
+//for reblog
+const int pressSensorPin = 1;
+int pressCurrentValue = 0;
+char altKey = KEY_LEFT_ALT;
+
 void setup() {
+  while(!Serial); //シリアルモニタが開いていたらループ開始
+  Serial.begin(9600); //シリアルポートを9600bpsで開く
  Keyboard.begin();
 }
 
 void loop() {
  // read Flex sensor value
  int value = analogRead(sensorPin);
- 
+ Serial.println(value); //とりあえず出力
  if (value > 430 && currentValue <= 430) {
    // LIKE!
    Keyboard.write('l');
  }
- currentValue = value;
- 
+ currentValue = value; 
  // wait
  delay(100);
+ 
+ //reblog
+ int pressVal = analogRead(pressSensorPin); //曲センサーの値を読み取る
+  Serial.println(pressVal); //とりあえず出力
+  if(pressVal > 900 && pressCurrentValue <= 900){
+    //Reblog
+    Keyboard.press(altKey);
+    Keyboard.press('r');
+    Keyboard.releaseAll();
+  }
+  pressCurrentValue = pressVal;
+  delay(100);
+ 
+ 
  
 }
